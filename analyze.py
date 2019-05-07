e688819f0102fec7f200486e76ed7bfc0d3088c6
Kristian Flikka <kristian.flikka@gmail.com>
2019-05-07 11:42:07 +0200

    Vary resampling frequency and method

--- analyze.py
+++ analyze.py
@@ -7,9 +7,12 @@ import matplotlib.pyplot as plt
 from gordo_components import serializer
 
 
-def read_and_resample(filename, resolution):
+def read_and_resample(filename, resolution, aggregation_method="mean"):
     raw_frame = pd.read_hdf(filename)
-    return raw_frame.resample(resolution).mean()
+    if aggregation_method == "max":
+        return raw_frame.resample(resolution).max()
+    elif aggregation_method == "mean":
+        return raw_frame.resample(resolution).mean()
 
 
 def make_anomalies(input_outputs, numsensors, normalizer=None):
@@ -31,7 +34,7 @@ def make_anomalies(input_outputs, numsensors, normalizer=None):
 
 def build_model_return_anomalies(resampled_dataframe):
     config = yaml.load(
-        """ 
+        """
         sklearn.pipeline.Pipeline:
             steps:
               - sklearn.preprocessing.data.MinMaxScaler
@@ -49,28 +52,32 @@ def build_model_return_anomalies(resampled_dataframe):
 
     print("Run data through model for prediction")
 
-    predict_from = train_until * 3
+    predict_from = 0
     res = model.transform(resampled_dataframe.iloc[predict_from:])
-
-    anomalies = make_anomalies(res, 4, normalizer=pipe.steps[0][1])
+    anomalies = make_anomalies(res, 4)
     anomalies = pd.DataFrame(anomalies, index=resampled_dataframe.index[predict_from:])
     return anomalies
 
 
-resamples_for_model = ["1S", "30S", "1T"]
+resamples_for_model = ["1S", "1T"]
+aggregation_methods = ["max", "mean"]
+plotnum = 0
+f, axarr = plt.subplots(
+    len(aggregation_methods) * len(resamples_for_model) + 1, sharex=True
+)
+resampled_original_data = read_and_resample("2nd_test.hdf", "1S")
+axarr[plotnum].plot(resampled_original_data, linewidth=1, label="sensor_data_1S_mean")
+axarr[plotnum].legend(loc="upper left")
+plotnum += 1
+for aggregation_method in aggregation_methods:
+    for interval in resamples_for_model:
+        print(f"Build model for data resampled with {interval} interval")
+        resampled = read_and_resample("2nd_test.hdf", interval)
+        anomalies = build_model_return_anomalies(resampled)
+        axarr[plotnum].plot(
+            anomalies, label=interval + "-" + aggregation_method + "-model"
+        )
+        axarr[plotnum].legend(loc="upper left")
+        plotnum += 1
 
-resulting_resampling = "5T"
-resampled_original_data = read_and_resample("2nd_test.hdf", resulting_resampling)
-plt.figure()
-plt.plot(resampled_original_data, linewidth=0.5)
-for interval in resamples_for_model:
-    print(f"Build model for data resampled with {interval} interval")
-    resampled = read_and_resample("2nd_test.hdf", interval)
-    anomalies = (
-        build_model_return_anomalies(resampled).resample(resulting_resampling).mean()
-    )
-
-    plt.plot(anomalies, linewidth=2, linestyle="dashed", label=interval + "-model")
-
-plt.legend(loc="upper left")
 plt.show()
