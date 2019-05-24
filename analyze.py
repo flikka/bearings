f8b96be354a85370b6bae1bfc48281d52b0bfbfb
Kristian Flikka <kristian.flikka@gmail.com>
2019-05-24 15:55:21 +0200

    Update with different resampling

--- analyze.py
+++ analyze.py
@@ -41,43 +41,47 @@ def build_model_return_anomalies(resampled_dataframe):
               - gordo_components.model.models.KerasAutoEncoder:
                   kind: feedforward_hourglass
                   epochs: 3
-                  batch_size: 100
+                  batch_size: 10
         """
     )
     pipe = serializer.pipeline_from_definition(config)
 
     print("Fit model to first part of data")
-    train_until = int(len(resampled_dataframe) / 4)
+    train_until = int(len(resampled_dataframe) / 5)
     model = pipe.fit(resampled_dataframe.iloc[:train_until])
 
     print("Run data through model for prediction")
-
-    predict_from = 0
-    res = model.transform(resampled_dataframe.iloc[predict_from:])
+    res = model.transform(resampled_dataframe)
     anomalies = make_anomalies(res, 4)
-    anomalies = pd.DataFrame(anomalies, index=resampled_dataframe.index[predict_from:])
-    return anomalies
+    anomalies = pd.DataFrame(anomalies, index=resampled_dataframe.index)
+    anomalies_mean_training = anomalies.iloc[:train_until].mean()[0]
+    return anomalies, anomalies_mean_training
+
+def analyse_with_gordo():
+    resamples_for_model = ["1S", "1T"]
+    aggregation_methods = ["max", "mean"]
+    plotnum = 0
+    f, axarr = plt.subplots(
+        len(aggregation_methods) * len(resamples_for_model) + 1, sharex=True
+    )
+    resampled_original_data = read_and_resample("2nd_test.hdf", "1S")
+    axarr[plotnum].plot(resampled_original_data, linewidth=1, label="sensor_data_1S_mean")
+    axarr[plotnum].legend(loc="upper left")
+    plotnum += 1
+    for aggregation_method in aggregation_methods:
+        for interval in resamples_for_model:
+            print(f"Build model for data resampled with {interval} interval and method {aggregation_method}")
+            resampled = read_and_resample("2nd_test.hdf", interval, aggregation_method=aggregation_method)
+            anomalies, avg_train_anomaly = build_model_return_anomalies(resampled)
+            anomalies = anomalies.rolling("30T").mean()
+            axarr[plotnum].plot(
+                anomalies, label=interval + "-" + aggregation_method + "-model"
+            )
+            axarr[plotnum].axhline(avg_train_anomaly, color='r')
+            axarr[plotnum].legend(loc="upper left")
+            plotnum += 1
+
+    plt.show()
 
 
-resamples_for_model = ["1S", "1T"]
-aggregation_methods = ["max", "mean"]
-plotnum = 0
-f, axarr = plt.subplots(
-    len(aggregation_methods) * len(resamples_for_model) + 1, sharex=True
-)
-resampled_original_data = read_and_resample("2nd_test.hdf", "1S")
-axarr[plotnum].plot(resampled_original_data, linewidth=1, label="sensor_data_1S_mean")
-axarr[plotnum].legend(loc="upper left")
-plotnum += 1
-for aggregation_method in aggregation_methods:
-    for interval in resamples_for_model:
-        print(f"Build model for data resampled with {interval} interval")
-        resampled = read_and_resample("2nd_test.hdf", interval)
-        anomalies = build_model_return_anomalies(resampled)
-        axarr[plotnum].plot(
-            anomalies, label=interval + "-" + aggregation_method + "-model"
-        )
-        axarr[plotnum].legend(loc="upper left")
-        plotnum += 1
-
-plt.show()
+analyse_with_gordo()
